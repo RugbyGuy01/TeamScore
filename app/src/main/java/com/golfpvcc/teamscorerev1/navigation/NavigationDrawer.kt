9c11cea6ef56c05902f103335ef1ad6debb00cfa
RugbyGuy01 <goodmail@golfpvcc.com>
2023-12-18 09:28:22 -0500

    Vacation

---
+++ app/src/main/java/com/golfpvcc/teamscorerev1/navigation/NavigationDrawer.kt
@@ -0,0 +1,20 @@
+package com.golfpvcc.teamscorerev1.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NativationHeader() {
+    Box(modifier = Modifier.fillMaxWidth()
+        .padding(vertical = 64.dp),
+        contentAlignment = Alignment.Center
+    ){
+
+    }
+
+}
